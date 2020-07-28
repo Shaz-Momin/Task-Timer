b148cdf85f8c21a7b897e23bc7d0b7d78ae92186
Shaz Momin <shazmomin@MacBook-Pro.local>
2020-07-28 13:01:52 -0500

    Updates

---
+++ precache-manifest.7bcb78cfac508cadf65b09b027aa1b7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249e9025f15e36c50f4f2c6b313fea53",
+    "url": "/Task-Timer/index.html"
+  },
+  {
+    "revision": "f06dadb054e415d8ea52",
+    "url": "/Task-Timer/static/css/main.efc9809c.chunk.css"
+  },
+  {
+    "revision": "e3b3f530af9ae8a0c4e9",
+    "url": "/Task-Timer/static/js/2.bb15c782.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Task-Timer/static/js/2.bb15c782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f06dadb054e415d8ea52",
+    "url": "/Task-Timer/static/js/main.f188731c.chunk.js"
+  },
+  {
+    "revision": "f664df7c9d9810399428",
+    "url": "/Task-Timer/static/js/runtime-main.162b653c.js"
+  }
+]);
